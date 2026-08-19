c26f9961ad6fe20d576514c4c2d7df8858f259ba
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-19 00:36:52 -0500

    Quinto intento para arreglar función de eliminar_duplicados

--- src/data/data.py
+++ src/data/data.py
@@ -50,7 +50,7 @@ class Data:
         lisindup = []
         for i in lista:
             if i not in lisindup:
-                lisindup.insert(0, lista[i])
+                lisindup.append(0, i)
         return lisindup
     
     def merge_ordenado(self, lista1, lista2):
