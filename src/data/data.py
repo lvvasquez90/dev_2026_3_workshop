5e3b4aaadf5abedb7a3003d5c8bb6b7723bd6ab4
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-26 01:12:46 -0500

    Sexto intento por arreglar matriz_transpuesta

--- src/data/data.py
+++ src/data/data.py
@@ -222,7 +222,7 @@ class Data:
         if cantfil == 0 or cantcol == 1:
             return matriz
         else:
-            trapue = [[0 for _ in range(cantcol)] for _ in range(cantfil)]
+            trapue = [[0 for _ in range(cantfil)] for _ in range(cantcol)]
             
             for i in range(cantfil):
                 for j in range(cantcol):
