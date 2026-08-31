42ac987bc642b5b360474ec414a9492cda6bbfce
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-31 01:10:57 -0500

    Primer intento para arreglar perimetro_rectangulo

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -31,7 +31,7 @@ class Geometria:
         Returns:
             float: Perímetro del rectángulo
         """
-        pass
+        return 2 * (base + altura)
     
     def area_circulo(self, radio):
         """
