2b68f311058a308199fc3886fec99d08b37cb552
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-31 01:23:11 -0500

    Primer intento para arreglar es_triangulo_valido

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -101,7 +101,13 @@ class Geometria:
         Returns:
             bool: True si los lados pueden formar un triángulo, False en caso contrario
         """
-        pass
+        if lado1 < 0 or lado2 < 0 or lado3 < 0:
+            return False
+        else:
+            if (lado1 + lado2) > lado3:
+                return True
+            else:
+                return False
     
     def area_trapecio(self, base_mayor, base_menor, altura):
         """
