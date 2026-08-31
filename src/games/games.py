c2a05959d2502025072ffa74834da9209054ab80
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-30 23:56:58 -0500

    17 intento para arreglar generar_combinacion_mastermind

--- src/games/games.py
+++ src/games/games.py
@@ -113,7 +113,7 @@ class Games:
                 combi.append(colores_disponibles[0])
             elif longitud >= lon:
                 while longitud > 0:
-                    ind = random.randint(0, lon)
+                    ind = random.randint(0, lon - 1)
                     combi.append(colores_disponibles[ind])
                     longitud -= 1
             return combi
