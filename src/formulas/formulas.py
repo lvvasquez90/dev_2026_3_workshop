e6b90a89c1b7528e9b21d81957fd41fd6a69aaa4
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-30 19:16:18 -0500

    Primer intento por arreglas mruv_velocidad

--- src/formulas/formulas.py
+++ src/formulas/formulas.py
@@ -59,7 +59,8 @@ class Formulas:
         Ejemplo:
             mruv_velocidad(2, 1, 3) -> 5.0
         """
-        pass
+        velfinmov = velocidad_inicial + (aceleracion * tiempo)
+        return velfinmov
 
     def fuerza_newton(self, masa, aceleracion):
         """
