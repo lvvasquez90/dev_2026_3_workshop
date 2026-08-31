2f25e7b0afbb6649ae54a93d4f45733cd77a73b1
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-31 01:44:46 -0500

    Segundo intento para arreglar area_superficie_cilindro

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -261,7 +261,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cilindro
         """
-        return 2 * 3.1416 * radio * altura
+        return 2 * 3.1416 * radio * (altura + radio)
     
     def distancia_entre_puntos(self, x1, y1, x2, y2):
         """
