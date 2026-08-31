e73d0c39c55cb9e8cf7822a3ad4d271f4191ea0a
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-30 21:16:41 -0500

    Primer intento por arreglar piedra_papel_tijera

--- src/games/games.py
+++ src/games/games.py
@@ -15,7 +15,16 @@ class Games:
             - Tijera vence a papel
             - Papel vence a piedra
         """
-        pass
+        if jugador1 == jugador2:
+            return "empate"
+        elif (
+            (jugador1 == "piedra" and jugador2 == "tijera") or
+            (jugador1 == "tijera" and jugador2 == "papel") or
+            (jugador1 == "papel" and jugador2 == "piedra")
+        ):
+            return "jugador1"
+        else:
+            return "jugador2"
     
     def adivinar_numero_pista(self, numero_secreto, intento):
         """
