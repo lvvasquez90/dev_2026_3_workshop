1031818627edf6798d19fd72c192a561427247b7
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-31 01:31:46 -0500

    Primer intento para arreglar area_hexagono_regular

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -172,7 +172,7 @@ class Geometria:
         Returns:
             float: Área del hexágono regular
         """
-        pass
+        return ((6 * lado) * apotema) / 2
     
     def perimetro_hexagono_regular(self, lado):
         """
