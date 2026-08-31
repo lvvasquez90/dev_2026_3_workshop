e25f64404433679717bfc69d0ce23e26ab5aa94d
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-31 01:51:13 -0500

    Primer intento para arreglar pendiente_recta

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -310,7 +310,9 @@ class Geometria:
         Returns:
             float: Pendiente de la recta
         """
-        pass
+        y = y2 - y1
+        x = x2 - x1
+        return y / x
     
     def ecuacion_recta(self, x1, y1, x2, y2):
         """
