a3599a8af84407f5b8ef5102414c532883006c88
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-30 22:36:19 -0500

    Un pequeño arreglo

--- src/games/games.py
+++ src/games/games.py
@@ -83,7 +83,7 @@ class Games:
             return "X"
         elif ganador(tablero, "O"):
             return "O"
-        elif not tablero:
+        elif " " in tablero:
             return "continua"
         elif (ganador(tablero, "O") == False and ganador(tablero, "X") == False):
             return "empate"
