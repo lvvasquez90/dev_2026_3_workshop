ea5acc641710e5f1febcf6cbeccaa516c39cd910
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-08-30 23:52:56 -0500

    14 intento para arreglar generar_combinacion_mastermind

--- src/games/games.py
+++ src/games/games.py
@@ -112,9 +112,10 @@ class Games:
                 combi.append(colores_disponibles[0])
             elif longitud >= lon:
                 while longitud > 0:
-                    ind = id([]) % lon
+                    ind = id(object()) % lon
                     combi.append(colores_disponibles[ind])
                     longitud -= 1
+                    ind -= 1
             return combi
     
     def validar_movimiento_torre_ajedrez(self, desde_fila, desde_col, hasta_fila, hasta_col, tablero):
