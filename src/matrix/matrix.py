6d483798ee84cd8497246159ccb4248ad2a62727
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-09-02 03:03:11 -0500

    Primer intento por arreglar es_cuadrada

--- src/matrix/matrix.py
+++ src/matrix/matrix.py
@@ -137,7 +137,9 @@ class Matrix:
             es_cuadrada([[1, 2], [3, 4]]) -> True
             es_cuadrada([[1, 2, 3], [4, 5, 6]]) -> False
         """
-        pass
+        if len(matriz) != len(matriz[0]):
+            return False
+        return True
 
     def es_simetrica(self, matriz):
         """
