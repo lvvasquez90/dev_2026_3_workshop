d31996c38edfdd26d1558d71be76d7827bfb947d
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-09-02 00:07:12 -0500

    15 intento por arreglar fibonacci

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -25,7 +25,7 @@ class Magic:
             while len(sec) < n:
                 sec.append(a)
                 a, fibo = fibo, a + fibo
-            return sec[-1]
+            return a
     
     def secuencia_fibonacci(self, n):
         """
