531d5ba6485b1198fe5ff61677bffbef37f71d79
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-09-02 00:41:06 -0500

    Primer y medio intento por arreglar factorial

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -138,7 +138,7 @@ class Magic:
         facto = 0
         if n == 0 or n == 1:
             return 1
-        for i in range(n):
+        for i in range(1, n):
             facto *= i
         return facto
     
