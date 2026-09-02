d3329de42aab901cb736e4bdbd0ca28a9c0fcbfe
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-09-02 01:29:22 -0500

    Primer intento por arreglar mcm

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -208,7 +208,10 @@ class Magic:
         Returns:
             int: El mínimo común múltiplo de a y b
         """
-        pass
+        if a == 0 or b == 0:
+            return 0
+        else:
+            return (a * b) // self.mcd(a, b)
     
     def suma_digitos(self, n):
         """
