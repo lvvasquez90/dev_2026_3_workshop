0e0cea4c39eb67353c5f98f34af998ba7c9fc8a1
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-09-02 00:20:06 -0500

    Cuarto intento por arreglar es_primo

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -55,7 +55,9 @@ class Magic:
         Returns:
             bool: True si n es primo, False en caso contrario
         """
-        if n <= 1 and n % 2 == 0:
+        if n <= 1: 
+            return False
+        elif n % 2 == 0:
             return False
         else:
             for i in range(2, n):
