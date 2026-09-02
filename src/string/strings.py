5b752769e7c3bd75473800391d1750cf545c8551
Laura Valentina Vásquez Prieto <lvvasquez90@ucatolica.edu.co>
2026-09-02 14:27:28 -0500

    Segundo intento por arreglar contar_consonantes

--- src/string/strings.py
+++ src/string/strings.py
@@ -49,7 +49,7 @@ class Strings:
         vcm = [vo.upper() for vo in voc]
         sma = 0
         for i in texto:
-            if i in voc or i in vcm:
+            if i in voc and i in vcm:
                 sma += 1
         return sma
     
@@ -67,7 +67,7 @@ class Strings:
         vcm = [vo.upper() for vo in voc]
         sma = 0
         for i in texto:
-            if i not in voc or i not in vcm:
+            if i not in voc and i not in vcm:
                 sma += 1
         return sma
     
